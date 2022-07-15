ec6e9c03c04f72cbdf1a711f260082795c666e40
Gennady Badinov <gennady.badinov@gmail.com>
2022-07-15 08:52:10 +0300

    Refactor code

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind(("localhost", 5000))
 server_socket.listen()
 
-while True:
-    print("Before .accept()")
-    client_socket, address = server_socket.accept()
-    print("Connection from", address)
 
+def accept_connection(server_socket):
+    while True:
+        print("Before .accept()")
+        client_socket, address = server_socket.accept()
+        print("Connection from", address)
+        send_message(client_socket)
+
+
+def send_message(client_socket):
     while True:
         print("Before .recv()")
         request = client_socket.recv(4096)
@@ -18,3 +23,9 @@ while True:
         else:
             response = "Hello world\n".encode()
             client_socket.send(response)
+
+    client_socket.close()
+
+
+if __name__ == "__main__":
+    accept_connection(server_socket)
