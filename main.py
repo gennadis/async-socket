7871061d4a581802721ae11d0f84016c8adeb5a7
Gennady Badinov <gennady.badinov@gmail.com>
2022-07-15 09:52:45 +0300

    Refactor with callbacks

--- main.py
+++ main.py
@@ -1,41 +1,49 @@
 import socket
-from select import select
+import selectors
 
-to_monitor = []
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(("localhost", 5000))
-server_socket.listen()
+selector = selectors.DefaultSelector()
+
+
+def server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(("localhost", 5000))
+    server_socket.listen()
+
+    selector.register(
+        fileobj=server_socket, events=selectors.EVENT_READ, data=accept_connection
+    )
 
 
 def accept_connection(server_socket):
     client_socket, address = server_socket.accept()
     print("Connection from", address)
 
-    to_monitor.append(client_socket)
+    selector.register(
+        fileobj=client_socket, events=selectors.EVENT_READ, data=send_message
+    )
 
 
 def send_message(client_socket):
     print("Before .recv()")
     request = client_socket.recv(4096)
+
     if request:
         response = "Hello world\n".encode()
         client_socket.send(response)
     else:
+        selector.unregister(client_socket)
         client_socket.close()
 
 
 def event_loop():
     while True:
-        ready_to_read, _, _ = select(to_monitor, [], [])  # read, write, errors
-
-        for sock in ready_to_read:
-            if sock is server_socket:
-                accept_connection(sock)
-            else:
-                send_message(sock)
+        events = selector.select()
+        for key, _ in events:
+            callback = key.data
+            callback(key.fileobj)
 
 
 if __name__ == "__main__":
-    to_monitor.append(server_socket)
+    server()
     event_loop()
